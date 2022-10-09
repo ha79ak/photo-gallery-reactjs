0e8307e5dfd5647f0756c9449897ccc4b4c1ca87
Hakan Akkaya <44675058+HakanPuente@users.noreply.github.com>
2022-10-09 13:23:19 +0300

    Signin and Signup pages created

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,8 @@
+import { Box, TextField, CssBaseline, Button, Container, Link, Avatar, Typography } from '@mui/material'
 import React, { useEffect } from 'react'
 import { signIn } from '../firebase/signInAndSignUp'
+
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 
 const SignIn = () => {
 
@@ -13,7 +16,57 @@ const SignIn = () => {
     //     })
     // }, [])
   return (
-    <div>Hi, signIn</div>
+    <>
+    <CssBaseline />
+    <Container maxWidth="xs">
+    <Box
+      component="form"
+      sx={{mt: 4}}
+      // style={{
+      //   display: "flex",
+      //   flexDirection: "column"
+      // }}
+    >
+      <Avatar sx={{ mx:"auto", bgcolor:"#589a9f"}}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography variant="h5" sx={{textAlign: "center"}}>Sign In</Typography>
+      <TextField 
+        fullWidth 
+        margin="normal" 
+        label="Email" 
+        required 
+        autoComplete="email" 
+        autoFocus
+      />
+      <TextField 
+        fullWidth 
+        margin="normal" 
+        label="Password" 
+        required 
+        type="password" 
+      />
+      <Button 
+        type='Submit' 
+        variant="contained" 
+        fullWidth 
+        sx={{mt: 2, bgcolor:"#589a9f" }}
+      >Sign In
+      </Button>
+    <Box 
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        mt: 2
+      }}
+    >
+      <Link sx={{color:"#589a9f"}}>Forgot Password?</Link>
+      <Link sx={{color:"#589a9f"}}>Don't have an account ? Sign Up</Link>
+      </Box>
+    </Box>
+  </Container>
+  </>
   )
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,8 @@
+import { Box, TextField, CssBaseline, Button, Container, Link, Avatar, Typography } from '@mui/material'
 import React, { useEffect } from 'react'
 import { signUp } from '../firebase/signInAndSignUp'
+
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 
 const SignUp = () => {
 
@@ -13,7 +16,58 @@ const SignUp = () => {
     //     })
     // }, [])
   return (
-    <div>Yo, signUp</div>
+    <>
+    <CssBaseline />
+    <Container maxWidth="xs">
+    <Box
+      component="form"
+      sx={{mt: 4}}
+    >
+      <Avatar sx={{ mx:"auto", bgcolor:"#589a9f"}}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography variant="h5" sx={{textAlign: "center"}}>Sign Up</Typography>
+      <TextField 
+        fullWidth 
+        margin="normal" 
+        label="Name" 
+        required 
+        autoComplete="name" 
+        autoFocus
+      />
+      <TextField 
+        fullWidth 
+        margin="normal" 
+        label="Email" 
+        required 
+      />
+      <TextField 
+        fullWidth 
+        margin="normal" 
+        label="Password" 
+        required 
+        type="password" 
+      />
+      <Button 
+        type='Submit' 
+        variant="contained" 
+        fullWidth 
+        sx={{mt: 2, bgcolor:"#589a9f" }}
+      >Sign Up
+      </Button>
+    <Box 
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        mt: 2
+      }}
+    >
+      <Link sx={{color:"#589a9f"}}>Already have an account? Sign In</Link>
+      </Box>
+    </Box>
+  </Container>
+  </>
   )
 }
 
